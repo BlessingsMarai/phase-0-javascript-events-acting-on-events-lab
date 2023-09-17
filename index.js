ddf68a3f08dc5e57d35b632884b3ded5dcd0d948
Blessings <blessings.marai@student.moringaschool.com>
2023-09-17 10:22:02 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Your code here
+document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft") {
+      moveDodgerLeft();
+    } else if (event.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
+  
+  function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left < (400 - 40)) { // Ensure dodger doesn't go off the right edge (400 is the width, and 40 is dodger's width)
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
